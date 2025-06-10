0f367862bbe7a3cf125e2b15ad12194802b5117f
Priti Aryal <40359121+PritiAryal@users.noreply.github.com>
2025-06-10 18:05:25 -0400

    Created Repository Layer

---
+++ medical-profile-service/src/main/java/com/priti/medicalprofileservice/repository/MedicalProfileRepository.java
@@ -0,0 +1,11 @@
+package com.priti.medicalprofileservice.repository;
+
+import com.priti.medicalprofileservice.model.MedicalProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MedicalProfileRepository extends JpaRepository<MedicalProfile, UUID> {
+}
