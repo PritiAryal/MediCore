85c09d4f9aef07c8bc6ed88617f38b720b2092ce
Priti Aryal <40359121+PritiAryal@users.noreply.github.com>
2025-06-10 17:31:08 -0400

    Added medical-profile-service

---
+++ medical-profile-service/src/main/java/com/priti/medicalprofileservice/MedicalProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.priti.medicalprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalProfileServiceApplication.class, args);
+    }
+
+}
