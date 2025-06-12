c01aa65c4bdb3c9c1743bb5849b69e1d45a19ddf
Priti Aryal <40359121+PritiAryal@users.noreply.github.com>
2025-06-12 15:37:27 -0400

    Added Integrate springdoc-openapi for API documentation; prepare controller for Swagger annotations

--- medical-profile-service/src/main/java/com/priti/medicalprofileservice/controller/MedicalProfileController.java
+++ medical-profile-service/src/main/java/com/priti/medicalprofileservice/controller/MedicalProfileController.java
@@ -4,6 +4,8 @@ import com.priti.medicalprofileservice.dto.MedicalProfileRequestDTO;
 import com.priti.medicalprofileservice.dto.MedicalProfileResponseDTO;
 import com.priti.medicalprofileservice.dto.validators.CreateMedicalProfileValidationGroup;
 import com.priti.medicalprofileservice.service.MedicalProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/medical-profiles")
+@Tag(name = "Medical Profile", description = "API related to managing medical profiles")
 public class MedicalProfileController {
     private final MedicalProfileService medicalProfileService;
 
@@ -23,24 +26,28 @@ public class MedicalProfileController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all medical profiles", description = "Retrieve a list of all medical profiles")
     public ResponseEntity<List<MedicalProfileResponseDTO>> getMedicalProfiles() {
         List<MedicalProfileResponseDTO> medicalProfiles = medicalProfileService.getMedicalProfiles();
         return ResponseEntity.ok().body(medicalProfiles);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new medical profile", description = "Create a new medical profile with the provided details")
     public ResponseEntity<MedicalProfileResponseDTO> createMedicalProfile(@Validated({Default.class, CreateMedicalProfileValidationGroup.class}) @RequestBody MedicalProfileRequestDTO medicalProfileRequestDTO) {
         MedicalProfileResponseDTO medicalProfileResponseDTO = medicalProfileService.createMedicalProfile(medicalProfileRequestDTO);
         return ResponseEntity.ok().body(medicalProfileResponseDTO);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing medical profile", description = "Update the details of an existing medical profile by ID")
     public ResponseEntity<MedicalProfileResponseDTO> updateMedicalProfile(@PathVariable UUID id, @Validated({Default.class}) @RequestBody MedicalProfileRequestDTO medicalProfileRequestDTO){
         MedicalProfileResponseDTO medicalProfileResponseDTO = medicalProfileService.updateMedicalProfile(id, medicalProfileRequestDTO);
         return ResponseEntity.ok().body(medicalProfileResponseDTO);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a medical profile", description = "Delete a medical profile by ID")
     public ResponseEntity<MedicalProfileResponseDTO> deleteMedicalProfile(@PathVariable UUID id){
         MedicalProfileResponseDTO medicalProfileResponseDTO = medicalProfileService.deleteMedicalProfile(id);
         return ResponseEntity.ok().body(medicalProfileResponseDTO);
